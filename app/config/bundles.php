91cdcb4ca2c491e221df4714826bd29b1f76fdba
Hanakhin <hanakhin.nounimassotte@gmail.com>
2024-07-06 12:14:45 +0200

    added tailwind

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -7,4 +7,5 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
