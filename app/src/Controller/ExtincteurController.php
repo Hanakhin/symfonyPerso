a03981f3c902966aad7e835d7d5700030aea51ee
Format_Pro <eymeric57160@hotmail.fr>
2024-07-12 10:46:53 +0200

    je bosse beaucoup oui oui aimerique

--- app/src/Controller/ExtincteurController.php
+++ app/src/Controller/ExtincteurController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Extincteur;
 use App\Form\ExtincteurTypeForm;
 use App\Repository\ExtincteurRepository;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
